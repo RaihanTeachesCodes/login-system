656512173f7af3dc7a4fa11b84744cbf8f5522d9
Raihan <73723527+raihancoder09@users.noreply.github.com>
2022-06-30 14:03:23 +0700

    html form completed

--- loginsys/login/views.py
+++ loginsys/login/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def main_login(request):
-    return HttpResponse("Hello world")
+    template = loader.get_template("login_page.html")
+    return HttpResponse(template.render({}, request))
+
+def detection(request):
+    pass
 
