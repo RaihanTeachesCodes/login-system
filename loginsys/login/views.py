dddf2cef3bddc8a999549411a365f71ed3fb8d84
Raihan <73723527+raihancoder09@users.noreply.github.com>
2022-06-30 14:09:10 +0700

    html form completed

--- loginsys/login/views.py
+++ loginsys/login/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from matplotlib.pyplot import connect
+import mysql.connector
 
 def main_login(request):
     template = loader.get_template("login_page.html")
